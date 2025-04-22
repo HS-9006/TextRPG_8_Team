6c51b4cd65cb5a4631cfaceb92b398e7a4c08845
HS-9006 <nsin99153@gmail.com>
2025-04-22 19:19:59 +0900

    update : 전투 시작 수정

--- StartBattle.cs
+++ StartBattle.cs
@@ -5,7 +5,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextRPG_8_team
+namespace TextRPG_8_Team
 {
     internal class StartBattle
     {
@@ -16,15 +16,16 @@ namespace TextRPG_8_team
             Console.WriteLine("전투시작");
 
             // 1. 몬스터 생성
-            List<몬스터> 몬스터 = RandomMonsters();
+            List<Monster> monster = RandomMonsters();
 
             // 2. 내 정보 출력
             player.PlayerStat();
 
             // 3. 몬스터 출력
-            foreach (var 몬스터 in 몬스터)
+            for(int i = 0; i < monster.Count; i++)
             {
-                몬스터.ShowInfo();
+                Monster m = monster[i];
+                Console.WriteLine($"{i + 1}. Lv {m.level}{m.name}(HP: {m.health})");
             }
 
             // 4. 행동 선택
@@ -34,25 +35,49 @@ namespace TextRPG_8_team
 
             if (action == "1")
             {
-                Console.WriteLine("플레이어가 공격을 헀습니다.");
+                Console.WriteLine("공격할 몬스터의 번호를 선택해주세요.");
+                string input = Console.ReadLine();
+
+                if(int.TryParse(input, out int index) && index > 0 && index <= monster.Count)
+                {
+                    Monster currentMonster = monster[index - 1];
+
+                    if (!currentMonster.isAlive)
+                    {
+                        Console.WriteLine("이미죽은 몬스터입니다.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"플레이어가 {currentMonster.name}을 공격했습니다.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("잘못 입력하셨습니다.");
+                }
             }
         }
 
-        private List<몬스터> RandomMonsters()
+        private List<Monster> RandomMonsters()
         {
-            List<몬스터> 몬스터목록 = new List<몬스터>();
+            List<Monster> monsterList = new List<Monster>();
             string[] names = { "미니언", "대포미니언", "공허충" };
             int[] levels = { 2, 5, 3 };
             int[] hps = { 15, 25, 10 };
+            int[] attacks = { 5, 10, 3 };
+            int[] defs = { 3, 5, 7 };
+            int[] speed = { 2, 2, 2 };
+            int[] exp = { 3, 2, 1 };
+            int[] gold = { 1, 2, 3 };
 
             int count = rand.Next(1, 5);
             for (int i = 0; i < count; i++)
             {
                 int idx = rand.Next(names.Length);
-                몬스터목록.Add(new 몬스터(names[idx], levels[idx], hps[idx]));
+                monsterList.Add(new Monster(names[idx], levels[idx], hps[idx], attacks[idx], defs[idx], speed[idx], exp[idx], gold[idx] ));
             }
 
-            return 몬스터목록;
+            return monsterList;
         }
     }
 }
