106194c3976e7a952758b7c3b13e1eba635d9ecb
ckdals652 <ckdals652@naver.com>
2025-04-22 16:44:48 +0900

    - add - StartPage.cs
    - add - StartPage.cs

---
+++ StartPage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_8_Team
+{
+    internal class StartPage
+    {
+        //시작 화면
+        public void StartGame(/*Player _player*/)
+        {
+            bool isGameEnd = false;
+
+            while (true)
+            {
+                if (isGameEnd) break;
+
+                Console.Clear();
+                Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n이제 전투를 시작할 수 있습니다.\n");
+                Console.WriteLine("1. 상태 보기\n2. 전투 시작\n3. 인벤토리\n4. 게임 종료\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">>");
+
+                bool isChoiceNum = int.TryParse(Console.ReadLine(), out int choiceNum);
+
+                //선택한 것이 숫자가 아니라면 실행
+                if (!isChoiceNum)
+                {
+                    Console.WriteLine("잘못된 입력입니다");
+                    Thread.Sleep(500);
+                    continue;
+                }
+                //1~3의 값이 아니라면 실행
+                if (choiceNum > 3 || choiceNum < 1)
+                {
+                    Console.WriteLine("잘못된 입력입니다");
+                    Thread.Sleep(500);
+                    continue;
+                }
+
+                StartChoice enumChoice = (StartChoice)choiceNum;
+
+                switch (enumChoice)
+                {
+                    case StartChoice.Status:
+                        //_player.PlayerStat();
+                        break;
+                    case StartChoice.Battle:
+                        break;
+                    case StartChoice.Inventory:
+                        break;
+                    case StartChoice.GameEnd:
+                        isGameEnd = true;
+                        break;
+                    //case StartChoice.Guild:
+                        //break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("게임이 종료되었습니다");
+        }
+
+        enum StartChoice
+        {
+            Status = 1,
+            Battle,
+            Inventory,
+            GameEnd,
+            //Guild
+        }
+    }
+}
