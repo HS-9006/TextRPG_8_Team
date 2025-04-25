81971141a021e74d3d1d315620df1fa761e1f92f
ckdals652 <ckdals652@naver.com>
2025-04-25 13:03:40 +0900

    - fix - 직업 선택지 이름 바꿨슴돠
    - fix - 직업 선택지 이름 바꿨슴돠

--- AboutQuest/QuestManager.cs
+++ AboutQuest/QuestManager.cs
@@ -28,12 +28,15 @@ namespace TextRPG_8_Team
         public List<Quest> QuestList = new List<Quest>();
         public List<Quest> QuestClaerList = new List<Quest>();
 
+        //퀘스트를 클리어한 퀘스트리스트에 욺겨주는 함수
         public void MoveClearList(Quest _quest)
         {
             _quest.isMoveClear = true;
             QuestClaerList.Add(_quest);
             QuestList.Remove(_quest);
         }
+
+        //퀘스트 이름을 적어주면 그 이름의 퀘스트를 찾아서 리턴해주는 함수
         public T SearchQuest<T>(string _questName) where T : Quest
         {
             return QuestManager.Instance.QuestList.FirstOrDefault(q => q.questName == _questName) as T;
